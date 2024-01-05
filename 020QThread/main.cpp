4a8f43057128ee1bb6468346e51cc5ddb6fedac0
3001ssw <3001ssw@naver.com>
2024-01-05 13:51:28 +0900

    complete 020QThread blog code

--- 020QThread/main.cpp
+++ 020QThread/main.cpp
@@ -2,17 +2,20 @@
 
 #include <QObject>
 #include <QDebug>
+#include <QThread>
 
 #include "mythread.h"
 
+// Thread 시작 시그널을 받기위한 함수
 void slot_started()
 {
-    qDebug() << Q_FUNC_INFO;
+    qDebug() << "--- slot function: " << Q_FUNC_INFO;
 }
 
+// Thread 종료 시그널을 받기위한 함수
 void slot_finished()
 {
-    qDebug() << Q_FUNC_INFO;
+    qDebug() << "--- slot function: " << Q_FUNC_INFO;
 }
 
 int main(int argc, char *argv[])
@@ -20,14 +23,19 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     MyThread mythread;
+    // qDebug() << "isRunning: " << mythread.isRunning(); // run?
+    // qDebug() << "isFinished: " << mythread.isFinished(); // Finished?
 
-    QObject::connect(&mythread, &MyThread::started, slot_started);
-    QObject::connect(&mythread, &MyThread::finished, slot_finished);
+    QObject::connect(&mythread, &MyThread::started, slot_started); // 쓰레드 시작 signal 함수 연결
+    QObject::connect(&mythread, &MyThread::finished, slot_finished); // 쓰레드 종료 signal 함수 연결
 
     mythread.start();
+    // qDebug() << "isRunning: " << mythread.isRunning(); // run?
+    // qDebug() << "isFinished: " << mythread.isFinished(); // Finished?
 
-    mythread.wait();
+    mythread.wait(); // 종료할 때까지 대기
+    // qDebug() << "isRunning: " << mythread.isRunning(); // run?
+    // qDebug() << "isFinished: " << mythread.isFinished(); // Finished?
 
-    qDebug() << "exit";
     return a.exec();
 }
--- 020QThread/mythread.cpp
+++ 020QThread/mythread.cpp
@@ -1,17 +1,21 @@
 #include "mythread.h"
 
 #include <QDebug>
+#include <QCoreApplication>
 
 MyThread::MyThread()
 {
-
 }
 
 void MyThread::run()
 {
+    qDebug() << "========== start thread";
+
     for (int i = 0 ; i < 10 ; i++)
     {
-        qDebug("count: %d", i);
-        msleep(100);
+        qDebug("========== count: %d", i);
+        msleep(1000); // 1초(1000msec) 슬립
     }
+
+    quit();
 }
