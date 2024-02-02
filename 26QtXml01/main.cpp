128000300557b5f692dc7a056b4e13a29b4260c7
3001ssw <3001ssw@naver.com>
2024-02-02 15:14:04 +0900

    read/write

--- 26QtXml01/main.cpp
+++ 26QtXml01/main.cpp
@@ -15,9 +15,10 @@
 
 #include <QFile>
 #include <QXmlStreamReader>
+#include <QXmlStreamWriter>
 #include <QDebug>
 
-void simpleReadXML(QFile *file)
+void readXML(QFile *file)
 {
     QXmlStreamReader reader(file);
     if (reader.readNextStartElement())
@@ -37,13 +38,52 @@ void simpleReadXML(QFile *file)
     }
 }
 
+void writeXML()
+{
+    QByteArray data;
+    QXmlStreamWriter writer(&data);
+    writer.setAutoFormatting(true);
+    writer.writeStartDocument();
+    {
+        writer.writeStartElement("tag");
+        {
+            writer.writeStartElement("tag1");
+            {
+                writer.writeAttribute("name", "one");
+                writer.writeCharacters("1");
+            }
+            writer.writeEndElement();
+
+            writer.writeStartElement("tag2");
+            {
+                writer.writeAttribute("name", "two");
+                writer.writeCharacters("2");
+            }
+            writer.writeEndElement();
+        }
+        writer.writeEndElement();
+    }
+    writer.writeEndDocument();
+
+    QFile file("./write_sample.xml");
+
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    file.write(data.data());
+    file.close();
+
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QFile file(":/sample.xml");
+    QFile file(":/read_sample.xml");
     file.open(QFile::ReadOnly | QFile::Text);
-    simpleReadXML(&file);
+    readXML(&file);
+    file.close();
+
+
+    writeXML();
 
     return a.exec();
 }
